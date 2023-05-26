220348fdf529f92421e33369b5c5086a44f1baa4
Niklas Knecht <knecht.n@gmx.de>
2023-05-26 10:59:58 +0200

    Included the Combination forecast code in main, updated dependecnies, and started to construct a multivariate rolling regression to compare to the combination forecasts.

--- R/expanding_regression.R
+++ R/expanding_regression.R
@@ -32,3 +32,40 @@ expanding_uni_regression <- function(y, x, date, m,...){
   
   return(res)
 }
+################################################################################
+# df <- data %>% 
+#   select(date = yyyyq, Index_excess_forward, all_of(predictive_features)) %>% 
+#   filter(between(date, as.yearqtr("1947 Q1"), as.yearqtr("2005 Q4")))
+# label <- "Index_excess_forward"
+# m <- 60
+
+expanding_multi_regression <- function(df, label, m,...){
+  
+  f <- paste0(label, " ~ .")
+  
+  # Calculate expanding window regression coefficients
+  res <- df %>% 
+    cbind(., df %>% 
+            select(-date) %>% 
+            rollapplyr(., width = seq_along(df$Index_excess_forward), FUN = function(d) {
+              d <- as.data.frame(d) %>% 
+                mutate_all(as.numeric)
+           
+              if(nrow(d) <= m){
+                return(rep(NA, ncol(df)-1))
+              } else{
+                reg <- lm(f, data = d)
+                return(coef(reg)) 
+              }
+              
+            }, by.column = FALSE)
+    )
+  
+  # Rename new variabels
+  colnames(res) <- c(colnames(df), "alpha", paste("ß_",colnames(df)[-c(1,2)]))
+  
+  # Calculate predicted values and residuals
+  res$y_hat <- res$alpha + rowSums(as.data.frame(as.matrix(res[,paste("ß_",colnames(df)[-c(1,2)])]) * as.matrix(res[,colnames(df)[-c(1,2)]])), na.rm = T)
+  res$epsilon <- res$y - res$y_hat
+  return(res)
+}
--- main.R
+++ main.R
@@ -13,6 +13,7 @@ data <- openxlsx::read.xlsx(xlsxFile = PATH_DATA) %>%
     Index_excess = (as.numeric(CRSP_SPvwx) - Rfree),
     Index_excess_forward = shift(as.numeric(CRSP_SPvwx) - Rfree, -1),
   ) %>% 
+  mutate_at(colnames(.)[-1], as.numeric) %>% 
   filter(!is.na(Index_excess)) %>% 
   mutate(
     Index_hist_mean = rollapplyr(Index_excess, width = seq_along(Index_excess), mean)
@@ -38,7 +39,8 @@ data <- openxlsx::read.xlsx(xlsxFile = PATH_DATA) %>%
     "TMS" = LTY - TBL,
     "DFY" = AAA - BAA,
     "DFR" = CORPR - LTR
-  )
+  ) %>% 
+  mutate_at(colnames(.)[-1], as.numeric)
 
 # long_sample: yyyyq >= "1965 Q1"
 # short_sample: yyyyq >= "2006 Q1"
@@ -50,7 +52,7 @@ data <- openxlsx::read.xlsx(xlsxFile = PATH_DATA) %>%
 predictive_features <- c("D/P", "D/Y", "E/P", "D/E", "SVAR", "B/M", "NTIS",
                          "TBL", "LTY", "LTR", "TMS", "DFY", "DFR", "INFL", "I/K")
 
-original_sample_reg_df <- predictive_features %>%
+univariate_forecast <- predictive_features %>%
   lapply(., function(x){
     print(paste0("Feature: ", x))
     res <- data[!is.na(as.numeric(data[[x]])),]
@@ -67,7 +69,7 @@ original_sample_reg_df <- predictive_features %>%
   }) %>% 
   rbindlist() %>% 
   left_join(., data %>% select(date = yyyyq, Index_hist_mean), by = c("date")) %>% 
-  filter(!is.na(alpha), between(date, as.yearqtr("1965 Q1"), as.yearqtr("2005 Q4"))) %>%
+  filter(!is.na(alpha), between(date, as.yearqtr("1947 Q1"), as.yearqtr("2005 Q4"))) %>%
   group_by(feature) %>% 
   mutate(
     epsilon_hist = Index_hist_mean - y, 
@@ -77,47 +79,84 @@ original_sample_reg_df <- predictive_features %>%
   
 
 # Simple mean combination forecast
-combination_forecast <- original_sample_reg_df %>% 
-  select(date, feature, y_hat) %>% 
-  pivot_wider(data=., names_from = feature, values_from = y_hat) %>% 
+t = 165
+m = 1
+s = c(m:(t-1))
+combination_forecast <- univariate_forecast %>% 
+  select(date,feature,y,y_hat,epsilon_hist) %>%
+  group_by(date) %>%
+  mutate(mean_weight = 1/n(),
+         mean_combination = sum(mean_weight * y_hat),
+         median_combination = median(y_hat),
+         trimmed_mean_weight = 1/(n()-2),
+         trimmed_mean_combination = sum(trimmed_mean_weight[c(-which.max(y_hat),-which.min(y_hat))]*y_hat[c(-which.max(y_hat),-which.min(y_hat))])) %>%
+  group_by(feature) %>%
+  filter(.,row_number() == s) %>%
+  mutate(discount0.9 = 0.9^(t-1-s),
+         phi0.9 = discount0.9 * (y-y_hat)^2,
+         discount1 = 1^(t-1-s),
+         phi1 = discount1 * (y-y_hat)^2) %>%
+  group_by(date) %>%
+  mutate(dmspe_weight0.9 = phi0.9^(-1)/sum(phi0.9^(-1)),    ####???
+         dmspe_combination0.9 = sum(dmspe_weight0.9 * y_hat),
+         dmspe_weight1 = phi1^(-1)/sum(phi1^(-1)),          ####???
+         dmspe_combination1 = sum(dmspe_weight1 * y_hat)) %>%
+  group_by(date) %>%
+  mutate(error1= cumsum((unique(epsilon_hist))^2-(unique(mean_combination-y))^2),
+         error2= cumsum((unique(epsilon_hist))^2-(unique(median_combination-y))^2),
+         error3= cumsum((unique(epsilon_hist))^2-(unique(trimmed_mean_combination-y))^2),
+         error4= cumsum((unique(epsilon_hist))^2-(unique(dmspe_combination0.9-y))^2),
+         error5= cumsum((unique(epsilon_hist))^2-(unique(dmspe_combination1-y))^2)) %>% 
+  select(date,error1,error2,error3,error4,error5) %>%
+  distinct(date, .keep_all = TRUE) 
+
+multivariate_forecast <- data %>% 
+  select(date = yyyyq, Index_excess_forward, all_of(predictive_features)) %>%
+  filter(between(date, as.yearqtr("1947 Q1"), as.yearqtr("2005 Q4"))) %>% 
+  expanding_multi_regression(df = ., label = "Index_excess_forward", m = 60) %>% 
+  left_join(., data %>% select(yyyyq, Index_hist_mean), by = c("date" = "yyyyq")) %>% 
+  filter(!is.na(epsilon)) %>% 
   mutate(
-    simple_mean = rowMeans(select(.,-date)),
-    simple_median = apply(select(.,-date),1,median),
-    trimmed_mean = apply(select(.,-date),1,function(x){
-      x[max(x)] <- 0
-      x[min(x)] <- 0
-      return(sum(x)/(length(x)-2))
-    })
-  ) %>% 
-  select(date, simple_mean, simple_median, trimmed_mean) %>% 
-  pivot_longer(data=., cols = c(simple_mean, simple_median, trimmed_mean),
-               names_to = "feature", values_to = "y_hat") %>%
-  left_join(.,
-            original_sample_reg_df %>% 
-              filter(feature == "D/P") %>% 
-              select(date, y, Index_hist_mean, epsilon_hist),
-            by = "date") %>% 
-  group_by(feature) %>% 
-  mutate(
-    epsilon = y-y_hat,
+    epsilon_hist = Index_hist_mean - Index_excess_forward, 
     Net_SSE = cumsum(epsilon_hist^2 - epsilon^2)
-  ) %>% 
-  ungroup(feature)
-  
+  )
+
 ################################################################################
 # Plots #
 ################################################################################
 # Univariate
-original_sample_reg_df %>% 
+univariate_forecast %>% 
     as.data.frame() %>% 
     ggplot(.) + 
     geom_line(aes(x = date, y = Net_SSE)) + 
     facet_wrap(~feature)
     
+multivariate_forecast %>% 
+  ggplot(.) +
+  geom_line(aes(x = date, y = Net_SSE))
 # Combination Forecast
-combination_forecast %>% 
-  as.data.frame() %>% 
+# combination_forecast %>% 
+#   mutate(
+#     Mean= cumsum(error1),
+#     Median = cumsum(error2),
+#     Trimmed_mean = cumsum(error3),
+#     DMSPE1.0 = cumsum(error5),
+#     DMSPE0.9 = cumsum(error4)
+#   ) %>%
+#   select(date, Mean, Median, Trimmed_mean, DMSPE1.0, DMSPE0.9) %>% 
+#   pivot_longer(data=., cols = colnames(.)[colnames(.)!="date"],
+#                names_to = "feature", values_to = "Net_SSE") %>% 
+#   ggplot(.) + 
+#   geom_line(aes(x = date, y = Net_SSE)) + 
+#   facet_wrap(~feature)
+
+data.frame(date = combination_forecast$date, Mean = cumsum(combination_forecast$error1),Median = cumsum(combination_forecast$error2),
+           Trimmed_mean = cumsum(combination_forecast$error3),
+           DMSPE1.0 = cumsum(combination_forecast$error5),
+           DMSPE0.9 = cumsum(combination_forecast$error4))%>%
+  select(date, Mean, Median, Trimmed_mean, DMSPE1.0, DMSPE0.9) %>% 
+  pivot_longer(data=., cols = colnames(.)[colnames(.)!="date"],
+               names_to = "feature", values_to = "Net_SSE") %>% 
   ggplot(.) + 
   geom_line(aes(x = date, y = Net_SSE)) + 
   facet_wrap(~feature)
-
